1c3d5c40adc23abe1e182f521f4222d2eaf2d683
AYKDM <minhthai23012002@gmail.com>
2022-12-11 18:03:57 +0800

    up

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.json("Hello World");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 3000");
 });
